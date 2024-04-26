b180fa8b5a0cbb659d4394e2d0aaaa07e555a471
fiona-giraffe <fi.mcsherry@gmail.com>
2024-04-26 15:30:24 -0400

    Resolve format errors

--- controller.py
+++ controller.py
@@ -5,11 +5,13 @@ import random
 
 vector = pygame.math.Vector2
 
-class Controller():
+
+class Controller:
     def __init__(self) -> None:
         self._jumping = False
         self._dead = False
         self._left_right = 0
+
     def update(self):
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -18,7 +20,10 @@ class Controller():
                 if event.key == pygame.K_RIGHT:
                     self._left_right = 0.5
                 if event.key == pygame.K_SPACE:
+                    print("jump")
                     self._jumping = True
+                else:
+                    self._jumping = False
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_SPACE:
                     self._jumping = False
@@ -27,6 +32,16 @@ class Controller():
                 if event.key == pygame.K_RIGHT and self._left_right == 0.5:
                     self._left_right = 0
             if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
-    
+                pygame.quit()
+                sys.exit()
+
+    @property
+    def jumping(self):
+        """
+        Returns the jumping bool as a private attribute
+        Args:
+            none
+        Returns:
+            If the player is jumping or not
+        """
+        return self._jumping
