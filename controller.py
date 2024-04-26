ab49aae7541b119092f913a1a0c0676ccc96084a
lilyWay27 <lilycwei2027@gmail.com>
2024-04-26 02:30:48 -0400

    movement bug fix

--- controller.py
+++ controller.py
@@ -22,7 +22,9 @@ class Controller():
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_SPACE:
                     self._jumping = False
-                if event.key == pygame.K_LEFT and event.key == pygame.K_RIGHT:
+                if event.key == pygame.K_LEFT and self._left_right == -0.5:
+                    self._left_right = 0
+                if event.key == pygame.K_RIGHT and self._left_right == 0.5:
                     self._left_right = 0
             if event.type == QUIT:
                     pygame.quit()
