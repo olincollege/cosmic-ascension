0e9f07c08568ca382bc758e0a6bec91976771db8
fiona-giraffe <fi.mcsherry@gmail.com>
2024-04-26 15:37:05 -0400

    Resolve format errors

--- view.py
+++ view.py
@@ -12,10 +12,7 @@ class View:
         self._model = model
 
     def draw(self, displaysurface):
-        displaysurface.fill((0,0,0))
+        displaysurface.fill((0, 0, 0))
         displaysurface.blit(self._model.player.surf, self._model.player.rect)
         for platform in self._model.platforms:
             displaysurface.blit(platform.surf, platform.rect)
-
-
-
