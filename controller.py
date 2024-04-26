c7eda9e48a4abee417aecd52e896929c2e9b9b36
lilyWay27 <lilycwei2027@gmail.com>
2024-04-26 02:18:35 -0400

    add resolve controller merge

--- controller.py
+++ controller.py
@@ -27,11 +27,4 @@ class Controller():
             if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
-        
-
-    def jump(self):
-        hits = pygame.sprite.spritecollide(
-            self._model._player, self._model._platforms, False
-        )
-        if hits:
-            self._velocity.y = -30
+    
