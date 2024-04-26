1bd76e9e8619841cad3224f7ce28acf43ff629be
fiona-giraffe <fi.mcsherry@gmail.com>
2024-04-26 13:01:08 -0400

    Add to test file

--- test_controller.py
+++ test_controller.py
@@ -19,3 +19,8 @@ from test_common import (
         check_abstract_method,
     ],
 )
+
+# Checks to run: 
+# private: velocity, model, acceleration
+# move method: left, right, and jump velocities
+# jump method: hitting a platform and not hitting one
