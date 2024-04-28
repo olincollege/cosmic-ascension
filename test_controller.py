f6981796d4dc77d93329089282f29d4d97700558
fiona-giraffe <fi.mcsherry@gmail.com>
2024-04-28 18:03:49 -0400

    Write unit test to check key event results

--- test_controller.py
+++ test_controller.py
@@ -1,26 +1,69 @@
+from controller import Controller
 from test_common import (
     check_private_var,
-    check_board_attribute,
-    check_board_property,
-    check_board_assignment,
+    check_class_attribute,
+    check_class_property,
     check_core_method,
-    check_abstract_method,
 )
+import pygame
+import pytest
+from pygame.locals import *
+
+# Test cases
+
+controller_updates = [
+    # Check if the left arrow event returns correct movement
+    (pygame.event.post[pygame.event.KEYDOWN.K_LEFT], -0.5, False),
+    # Check if the right arrow event returns correct movement
+    (pygame.event.post[pygame.event.KEYDOWN.K_RIGHT], 0.5, False),
+    # Check if the space bar event returns correct movement
+    (pygame.event.post[pygame.event.KEYDOWN.K_SPACE], 0.0, True),
+    # Check if left arrow is released movement halts
+    (pygame.event.post[pygame.event.KEYUP.K_LEFT], 0.0, False),
+    # Check if right arrow is released movement halts
+    (pygame.event.post[pygame.event.KEYUP.K_RIGHT], 0.0, False),
+    # Check if space bar is released movement halts
+    (pygame.event.post[pygame.event.KEYUP.K_SPACE], 0.0, False),
+]
 
 
-@pytest.mark.parametrize(
-    "func",
-    [
-        check_private_board,
-        check_board_attribute,
-        check_board_property,
-        check_board_assignment,
-        check_core_method,
-        check_abstract_method,
-    ],
-)
+@pytest.mark.parametrize("keystroke,x_move,y_move", controller_updates)
+def test_controller_updates(keystroke, x_move, y_move):
+    """
+    Tests that a specified keystroke in the events queue will return the
+    correct set of movements.
 
-# Checks to run: 
+    Args:
+        keystroke: A pygame event representing a simulated pressed key from the user.
+        x_move: A float representing the expected movement when pressing a key.
+        y_move: A bool representing the expected jump status when pressing a key.
+    """
+    instance = Controller()
+    # post the event
+    keystroke
+    x_move_result = instance.left_right
+    y_move_result = instance.jumping
+
+    # Check the correct type
+    assert isinstance(x_move_result, float)
+    assert isinstance(y_move_result, bool)
+
+    # Check the correct value result
+    assert x_move_result == x_move
+    assert y_move_result == y_move
+
+
+# @pytest.mark.parametrize(
+#     "func",
+#     [
+#         check_private_var,
+#         check_class_attribute,
+#         check_class_property,
+#         check_core_method,
+#     ],
+# )
+
+# Checks to run:
 # private: velocity, model, acceleration
 # move method: left, right, and jump velocities
 # jump method: hitting a platform and not hitting one
